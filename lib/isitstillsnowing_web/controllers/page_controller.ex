a8526e6182538b0775184e448350152ef84c6c66
JessePledger <JessePledger@gmail.com>
2018-01-05 23:51:12 -0500

    Inspect response for debugging

--- lib/isitstillsnowing_web/controllers/page_controller.ex
+++ lib/isitstillsnowing_web/controllers/page_controller.ex
@@ -1,13 +1,15 @@
 defmodule IsitstillsnowingWeb.PageController do
   use IsitstillsnowingWeb, :controller
 
+
   alias Isitstillsnowing.Weather
 
   plug RemoteIp
 
   def index(conn, _params) do
     ip = Tuple.to_list(conn.remote_ip) |> Enum.join(".")
-    {_status, weather_from_ip} = Weather.main(ip)
+    # Inspecting for debugging
+    {_status, weather_from_ip} = Weather.main(ip) |> IO.inspect
 
     render conn, "index.html", snowing: weather_from_ip.snowing
   end
